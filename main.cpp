c13a2586e1d8ffb8ea1ae0ed20adac334d808133
dmitrijgulacenkov <pingvin1959@yandex.ru>
2019-11-15 20:57:19 +0300

    Объединил поиск и добавление в одну функцию

--- main.cpp
+++ main.cpp
@@ -1,99 +1,92 @@
 #include <iostream>
-template <class T, class Cmp>
-class Tree{
+
+template<class T, class Cmp>
+class Tree {
 private:
 
-    struct node{
+    struct node {
         T key;
-        node* left= nullptr;
-        node* right= nullptr;
-        int height=1;
-        int num_of_childs=0;
-        node(T data){
-            key=data;
+        node *left = nullptr;
+        node *right = nullptr;
+        int height = 1;
+        int num_of_childs = 0;
+
+        node(T data) {
+            key = data;
         }
 
 
     };
 
-    int height(node* p)
-    {
-        return p?p->height:0;
+    int height(node *p) {
+        return p ? p->height : 0;
     }
 
-    int num_child(node* p){
-        return p?p->num_of_childs:0;
+    int num_child(node *p) {
+        return p ? p->num_of_childs : 0;
     }
 
-    void fixheight(node*& p)
-    {
+    void fixheight(node *&p) {
         int hl = height(p->left);
         int hr = height(p->right);
-        p->height = (hl>hr?hl:hr)+1;
+        p->height = (hl > hr ? hl : hr) + 1;
     }
 
-    int bfactor(node* p)
-    {
-        return height(p->right)-height(p->left);
+    int bfactor(node *p) {
+        return height(p->right) - height(p->left);
     }
 
     Cmp cmp;
 
 
-
-    void balance(node*& p){
-
+    void balance(node *&p) {
 
 
         fixheight(p);
-        if( bfactor(p)==2 )
-        {
-            if( bfactor(p->right) < 0 )
+        if (bfactor(p) == 2) {
+            if (bfactor(p->right) < 0)
                 rotateright(p->right);
-             rotateleft(p);
+            rotateleft(p);
         }
-        if( bfactor(p)==-2 )
-        {
-            if( bfactor(p->left) > 0  )
+        if (bfactor(p) == -2) {
+            if (bfactor(p->left) > 0)
                 rotateleft(p->left);
             rotateright(p);
         }
 
     }
 
-    void req_add(node*& nod,const T& key){
-        if (nod== nullptr){
-            nod=new node(key);
+    void req_add(node *&nod, const T &key) {
+        if (nod == nullptr) {
+            nod = new node(key);
             return;
         }
-        if (cmp(key,nod->key)){
-            nod->num_of_childs+=1;
-            req_add(nod->right,key);
-        }
-        else {
-            nod->num_of_childs+=1;
+        if (cmp(key, nod->key)) {
+            nod->num_of_childs += 1;
+            req_add(nod->right, key);
+        } else {
+            nod->num_of_childs += 1;
             req_add(nod->left, key);
         }
         balance(nod);
     }
 
-    void req_print(node* nod){
-        if (nod== nullptr)
+    void req_print(node *nod) {
+        if (nod == nullptr)
             return;
         req_print(nod->left);
-        std::cout<< nod->key << " ";
+        std::cout << nod->key << " ";
         req_print(nod->right);
     }
 
-    void rotateright(node*& p)
-    {
+    void rotateright(node *&p) {
 
-        node* q = p->left;
-        int A_num=num_child(q->left);
-        int B_num=num_child(q->right);
-        int C_num=num_child(p->right);
-        int p_num_childs=num_child(p);
-        int q_num_childs=num_child(q);
+        node *q = p->left;
+        int A_num = num_child(q->left);
+        int B_num = num_child(q->right);
+        int C_num = num_child(p->right);
+        int p_num_childs = num_child(p);
+        int q_num_childs = num_child(q);
 
 
         p->left = q->right;
@@ -101,80 +94,78 @@ private:
         fixheight(p);
         fixheight(q);
 
-        p=q;
-        p->num_of_childs=p_num_childs;
-        if (p->right!= nullptr) p->right->num_of_childs=q_num_childs-1;
-        if (p->left!= nullptr) p->left->num_of_childs=A_num;
-        if (p->right->right!= nullptr)    p->right->right->num_of_childs=C_num;
-        if (p->right->left!= nullptr)     p->right->left->num_of_childs=B_num;
+        p = q;
+        p->num_of_childs = p_num_childs;
+        if (p->right != nullptr) p->right->num_of_childs = q_num_childs - 1;
+        if (p->left != nullptr) p->left->num_of_childs = A_num;
+        if (p->right->right != nullptr) p->right->right->num_of_childs = C_num;
+        if (p->right->left != nullptr) p->right->left->num_of_childs = B_num;
 
     }
 
 
-    void rotateleft(node*& q)
-    {
+    void rotateleft(node *&q) {
 
-        node* p = q->right;
-        int A_num=num_child(q->left);
-        int B_num=num_child(p->left);
-        int C_num=num_child(p->right);
-        int p_num_childs=num_child(p);
-        int q_num_childs=num_child(q);
+        node *p = q->right;
+        int A_num = num_child(q->left);
+        int B_num = num_child(p->left);
+        int C_num = num_child(p->right);
+        int p_num_childs = num_child(p);
+        int q_num_childs = num_child(q);
 
 
         q->right = p->left;
-        p->left=q;
+        p->left = q;
         fixheight(q);
         fixheight(p);
-        q=p;
+        q = p;
 
-        q->num_of_childs=q_num_childs;
-        if(q->left!= nullptr) q->left->num_of_childs=p_num_childs-1;
-        if(q->right!= nullptr) q->right->num_of_childs=C_num;
-        if (q->left->left!= nullptr)    q->left->left->num_of_childs=A_num;
-        if (q->left->right!= nullptr)     q->left->right->num_of_childs=B_num;
+        q->num_of_childs = q_num_childs;
+        if (q->left != nullptr) q->left->num_of_childs = p_num_childs - 1;
+        if (q->right != nullptr) q->right->num_of_childs = C_num;
+        if (q->left->left != nullptr) q->left->left->num_of_childs = A_num;
+        if (q->left->right != nullptr) q->left->right->num_of_childs = B_num;
 
 
     }
 
-    node* findmin(node* p) // поиск узла с минимальным ключом в дереве p
-    {
-        return p->left?findmin(p->left):p;
-    }
 
-    node* removemin(node* p) // удаление узла с минимальным ключом из дерева p
+    node *removemin(node *p, node *&temp_data) // удаление узла с минимальным ключом из дерева p
     {
-        if( p->left==0 )
+        temp_data = p;
+        while (temp_data->left != nullptr) {
+            temp_data = temp_data->left;
+        }
+        if (p->left == 0)
             return p->right;
-        p->left = removemin(p->left);
+        p->left = removemin(p->left, temp_data);
         balance(p);
         return p;
     }
 
-    node* remove(node* p, int k) // удаление ключа k из дерева p
+    node *remove(node *p, int k) // удаление ключа k из дерева p
     {
-        if( !p ) return 0;
-        if( k < p->key ) {
+        if (!p) return 0;
+        if (k < p->key) {
             p->num_of_childs--;
             p->left = remove(p->left, k);
-        }
-        else if( k > p->key ) {
+        } else if (k > p->key) {
             p->num_of_childs--;
             p->right = remove(p->right, k);
-        }
-        else //  k == p->key
+        } else //  k == p->key
         {
-            node* q = p->left;
-            node* r = p->right;
+            node *q = p->left;
+            node *r = p->right;
             delete p;
-            if( !r ) return q;
-            node* min=findmin(r);
-            if (min->left!= nullptr && min->right!= nullptr && min->left->height>min->right->height) {
-                min->left = removemin(r);
+            if (!r) return q;
+            node *min;
+            node *test = removemin(r, min);
+
+            if (min->left != nullptr && min->right != nullptr && min->left->height > min->right->height) {
+                min->left = test;
                 min->right = q;
-            }
-            else{
-                min->right = removemin(r);
+            } else {
+                min->right = test;
                 min->left = q;
             }
             balance(min);
@@ -185,56 +176,54 @@ private:
     }
 
 
-    node* data= nullptr;
+    node *data = nullptr;
 
 public:
 
 
-
-    Tree(Cmp m_cmp){
-        cmp=m_cmp;
+    Tree(Cmp m_cmp) {
+        cmp = m_cmp;
     }
 
-    void add(const T& key){
-        req_add(data,key);
+    void add(const T &key) {
+        req_add(data, key);
     }
 
-    void print(){
+    void print() {
         req_print(data);
     }
 
-    void del(const T& k){
-        data=remove(data,k);
+    void del(const T &k) {
+        data = remove(data, k);
     }
-
-
-
-
 
 
 };
 
 
-class Comparator{
+class Comparator {
 public:
-    bool operator () (int a, int b){
-        return a>b;
+    bool operator()(int a, int b) {
+        return a > b;
     }
 
 };
 
-int main(){
+int main() {
     Comparator cmp;
-    Tree<int,Comparator> tr(cmp);
+    Tree<int, Comparator> tr(cmp);
 
-    for(int i=0;i<7;i++){
+    for (int i = 0; i < 7; i++) {
         tr.add(i);
     }
-    for (int i=0;i<4;i++) {
-        tr.del(i+3);
-        tr.print();
+    tr.print();
+    std::cout << std::endl;
+    for (int i = 0; i <= 4; i++) {
+        tr.del(i + 2);
         std::cout << std::endl;
+        tr.print();
     }
+
 
 
     //tr.print();
